3e3b40daa2e81122d220a0bcd70ecf1fb289f881
Attila Molnár <molnar.attila1@petrik.hu>
2025-11-27 12:16:55 +0100

    First Commit

--- src/Pages/Header.tsx
+++ src/Pages/Header.tsx
@@ -7,9 +7,9 @@ const Header = () => {
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-2">
                 <div className="container-fluid">
                     <a className="navbar-brand fs-4" href="#">
-                        <img src={logo} alt="Project Logo" width="40" height="40" className="d-inline-block align-middle me-2" />
-                        TechSupport
+                        <img src={logo} alt="Project Logo" width="70" height="70" className="d-inline-block align-middle me-2" />
                     </a>
+                    <h1 className="text-white display-4">TechSupport</h1>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
